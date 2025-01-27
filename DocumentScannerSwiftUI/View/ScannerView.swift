83e87ad882f1115a67c0453aa7ab2d3df51e5532
jorgemendiza <jorgemendiza@me.com>
2025-01-27 15:03:57 -0600

    implements navigation from home to scanner view

---
+++ DocumentScannerSwiftUI/View/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  DocumentScannerSwiftUI
+//
+//  Created by Jorge Mendizabal on 27/01/25.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
